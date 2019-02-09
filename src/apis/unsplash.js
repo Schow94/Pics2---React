1e537832a53ac41a18ec0f57919cf077cf465324
Stephen Chow <Schow94@users.noreply.github.com>
2019-02-09 14:09:01 -0800

    Update unsplash.js

--- src/apis/unsplash.js
+++ src/apis/unsplash.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 'Client-ID f2258104adfee6a6e1ed0f1ee074ced762899ab7ad9e7b7f99a77eca613d4fae'
+    Authorization: ''
   }
 
 });
